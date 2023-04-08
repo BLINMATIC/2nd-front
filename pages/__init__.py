c4ef3cc134cc94666f4231124b34df94e2436219
BLINMATIC <68463606+BLINMATIC@users.noreply.github.com>
2023-04-08 18:44:11 +0300

    Delete pages directory

--- pages/__init__.py
+++
@@ -1,7 +0,0 @@
-##################################
-# Made by BLINMATIC (Onur Çetin) #
-# Version 1.0 INDEV 07-APR-2023  #
-##################################
-
-from .main_menu import main_menu
-from .top_tab import top_tab
